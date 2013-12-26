2dcdcd4965cf5d3a3c8c21a76c500cdfaea5f2fb
dankeezer <dankeezer@gmail.com>
2013-12-26 13:22:14 -0500

    forcing heroku push with a comment

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,6 +1,8 @@
 class ArtworksController < ApplicationController
   before_filter :authenticate, :except => [:index]
 
+  #forcing heroku push whatever
+
   skip_before_filter  :verify_authenticity_token
 
   def index
