cad40d72401c6f64471a596d3c705a22120e2c66
Dan <dankeezer@gmail.com>
2014-01-01 17:57:58 -0500

    lightbox changes commented

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,6 +1,6 @@
 class ArtworksController < ApplicationController
   respond_to :html, :js, :json
-  before_filter :authenticate, :except => [:index]
+  before_filter :authenticate, :except => [:index, :lightbox]
   skip_before_filter  :verify_authenticity_token
 
   def index
