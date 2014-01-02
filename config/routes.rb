42204d6ff20218c3c3807b8f0586865a9339aa2e
Dan <dankeezer@gmail.com>
2014-01-01 20:01:06 -0500

    lightbox working better

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 ShainaGates::Application.routes.draw do
 
-  resources :artworks do
-    collection do
-      get 'lightbox/:id' => 'artworks#lightbox', as: 'lightbox'
-    end
-  end
+  resources :artworks
 
+  get ':id' => 'artworks#lightbox', as: 'lightbox'
   root 'artworks#index'
   get 'admin' => 'artworks#admin'
   post 'admin' => 'artworks#admin', as: 'sort_artworks'
