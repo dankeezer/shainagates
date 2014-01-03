d11ff0c9ec6143ddde7cd58bb273b664924103e5
Dan <dankeezer@gmail.com>
2014-01-02 21:50:05 -0500

    caption on top

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ ShainaGates::Application.routes.draw do
 
   resources :artworks do
     collection do
-      get 'lightbox/:id' => 'artworks#lightbox', as: 'lightbox'
+      get ':id' => 'artworks#lightbox', as: 'lightbox'
     end
   end
-  
+
   root 'artworks#index'
 
   get 'admin' => 'artworks#admin'
