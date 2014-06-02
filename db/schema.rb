37d72cd6c781e3bb8cd6d46d35eeca382ffdca4b
Dan Keezer <dankeezer@gmail.com>
2014-06-02 12:55:28 -0400

    removed filemaker convert

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131224042147) do
+ActiveRecord::Schema.define(version: 20131224041852) do
 
   create_table "artworks", force: true do |t|
     t.string   "name"
